215337b15632a4f3e40597fa0be43ddd109fbd19
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-02-22 22:00:50 +0100

    [ADD] Index.js - Ajout des appels aux routes et démarrage d'un serveur

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const express = require('express');
 
+const verifyToken = require('./middleware/verifyToken');
+const utilisateurRoute = require('./routes/utilisateurRoute');
+const blogRoute = require('./routes/blogRoute');
+const structureRoute = require('./routes/structureRoute');
+
 const app = express();
+app.use(express.json());
+
+//Appel des routes
+app.use(verifyToken);
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
+app.use('/structure', structureRoute);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Serveur démarré sur le port ${port}');
+});
