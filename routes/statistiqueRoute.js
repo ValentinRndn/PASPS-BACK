1c02af8473ea64ac85805222e2692a1666c01949
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-05-03 20:31:31 +0200

    [ADD] Statistiques algo

---
+++ routes/statistiqueRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+const statistiqueController = require('../controllers/statistiqueController');
+
+router.post('/createStatistique', statistiqueController.createStatistique);
+router.get('/getAllStatistiques', statistiqueController.getAllStatistiques);
+router.put('/updateStatistique/:id', statistiqueController.updateStatistique);
+
+module.exports = router;
+
