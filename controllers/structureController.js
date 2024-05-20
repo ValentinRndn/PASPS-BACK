d95853134599bd4f7be27aedcd9345c5816775bc
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-05-20 19:38:31 +0200

    [ADD] Controller getAllStructures

--- controllers/structureController.js
+++ controllers/structureController.js
@@ -21,3 +21,19 @@ exports.getStructureByCategorie = (req, res) => {
     });
 }
 
+//Controller getAllStructures
+exports.getAllStructures = (req, res) => {
+    const sql = 'SELECT * FROM structures';
+    const dbInstance = db.getInstance(); // Obtenir une instance de la classe Database
+
+    dbInstance.query(sql, (err, result) => {
+        if (err) {
+            console.error("Erreur lors de la récupération des structures", err);
+            return res.status(500).json({ message: "Erreur lors de la récupération des structures" });
+        } else {
+            const structures = result.map(structure => new Structure(structure.id, structure.antenne, structure.adresse, structure.coos_gps, structure.departement, structure.telephone, structure.mail, structure.horaires_lundi, structure.horaires_mardi, structure.horaires_mercredi, structure.horaires_jeudi, structure.horaires_vendredi, structure.horaires_samedi, structure.horaires_dimanche, structure.besoin, structure.categorie));
+
+            return res.status(200).json(structures);
+        }
+    });
+}
--- routes/structureRoute.js
+++ routes/structureRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const structureController = require('../controllers/structureController');
 
 router.get('/getStructureByCategorie/:categorie', structureController.getStructureByCategorie);
-// router.get('/getAllStructures', structureController.getAllStructures);
+router.get('/getAllStructures', structureController.getAllStructures);
 // router.post('/createStructure', structureController.createStructure);
 // router.put('/updateStructure/:id', structureController.updateStructure);
 // router.delete('/deleteStructure/:id', structureController.deleteStructure);
