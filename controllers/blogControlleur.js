00bf7682d620bdc6e24a80116389070e32ce7753
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-05-21 21:45:24 +0200

    [ADD] Route getLastBlog

--- controllers/blogControlleur.js
+++ controllers/blogControlleur.js
@@ -39,3 +39,24 @@ exports.getBlogById = (req, res) => {
         }
     });
 }
+
+
+//Controller getLastBlog
+exports.getLastBlog = (req, res) => {
+    const sql = 'SELECT * FROM blogs ORDER BY id DESC LIMIT 1';
+    const dbInstance = db.getInstance(); // Obtenir une instance de la classe Database
+
+    dbInstance.query(sql, (err, result) => {
+        if (err) {
+            console.error("Erreur lors de la récupération du dernier blog", err);
+            return res.status(500).json({ message: "Erreur lors de la récupération du dernier blog" });
+        } else {
+            if (result.length === 0) {
+                return res.status(404).json({ message: "Blog non trouvé" });
+            } else {
+                const blog = Blog.fromMap(result[0]);
+                return res.status(200).json(blog);
+            }
+        }
+    });
+}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -5,6 +5,7 @@ const blogController = require('../controllers/blogControlleur');
 
 router.get('/getBlogById/:id', blogController.getBlogById);
 router.get('/getAllBlogs', blogController.getAllBlogs);
+router.get('/getLastBlog', blogController.getLastBlog);
 // router.post('/createBlog', blogController.createBlog);
 // router.put('/updateBlog/:id', blogController.updateBlog);
 // router.delete('/deleteBlog/:id', blogController.deleteBlog);
